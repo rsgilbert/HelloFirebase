210c4cfcabbd514bfa66d1ede4abe22038b726b4
Gilbert <gssenyonjo@gmail.com>
2020-04-27 19:56:44 +0300

    Setup Firebase using assistant and firebasedocs. But it is not working. I can get a token but I cant receive a notification.

--- app/src/main/java/com/monstercode/hellofirebase/MainActivity.kt
+++ app/src/main/java/com/monstercode/hellofirebase/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.monstercode.hellofirebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +12,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val channelName = getString(R.string.default_notification_channel_name)
+        createChannel(channelId, channelName)
+
     }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Description goes here"
+
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+    }
+
 }
